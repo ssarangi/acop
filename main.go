8bb29a4b2ece7b66b569ba81b56d1219df12d99f
Satyajit Sarangi <ssarangi@users.noreply.github.com>
2022-07-06 06:36:47 +0000

    Initial commit

--- main.go
+++ main.go
@@ -1,29 +1,11 @@
 package main
 
 import (
-	"log"
-	"net/http"
-	"os"
+	"fmt"
 
-	"github.com/gin-gonic/gin"
 	_ "github.com/heroku/x/hmetrics/onload"
 )
 
 func main() {
-	port := os.Getenv("PORT")
-
-	if port == "" {
-		log.Fatal("$PORT must be set")
-	}
-
-	router := gin.New()
-	router.Use(gin.Logger())
-	router.LoadHTMLGlob("templates/*.tmpl.html")
-	router.Static("/static", "static")
-
-	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl.html", nil)
-	})
-
-	router.Run(":" + port)
+	fmt.Println("Hello World")
 }
